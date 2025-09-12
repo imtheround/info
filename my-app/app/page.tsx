8cf537c18dfa3806a2e93bdf8824aaa9ef8858f3
Round <138043319+imtheround@users.noreply.github.com>
2025-09-12 19:07:25 +0000

    first commit idk

---
+++ my-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+  <div className="w-full h-full flex items-center bg-pink-600 mx-auto">
+  <h1 className="tex"> hello there</h1>
+   <div className="mt-[20vh] mx-auto">gg</div>
+  </div>)
+}
