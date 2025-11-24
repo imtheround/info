9e274beb4d688116054e668891d543fbcb2ce311
Yuan Guanwei <2221661@cje.qc.ca>
2025-11-24 15:50:34 -0500

    did shit

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,18 +2,23 @@
 
 import React, { useEffect, useState } from "react";
 // for redirecting
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import MouseFollower from "../components/mousefollower";
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const diff = searchParams.get("diff");
   const [life, setLife] = useState<number>(8);
   const [guessed, setGuessed] = useState<boolean>(false);
   const [number, setNumber] = useState<number>(0);
   const [numberGuess, setNumberGuess] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const [maxRandomNumber, setMaxRandomNumber] = useState<number>(100);
+  const [ded, setded] = useState<boolean>(false);
   const router = useRouter();
 
   const getRandomInteger = (): number => {
-    return Math.floor(Math.random() * 100) + 1; // 1 - 100
+    return Math.floor(Math.random() * maxRandomNumber) + 1; // 1 - maxRandomNumber
   };
 
   const generateNumber = () => {
@@ -27,7 +32,9 @@ export default function Page() {
     setNumberGuess(v);
     console.log(v); 
   };
-
+  const button2submitCallback = () => {
+    router.push("/difficultySelect");
+  }
   const submitCallback = (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -61,11 +68,25 @@ export default function Page() {
     }
 
     if (newLife <= 0) {
-      setTimeout(() => {
-        router.push("/won?tried=" + encodeURIComponent(numberGuess));
-      });
+      setded(true)
     }
   };
+
+  useEffect(() => {
+    if (diff === "easy") {
+      setLife(12);
+      setMaxRandomNumber(60);
+    } else if (diff === "medium") {
+      setLife(9);
+      setMaxRandomNumber(100);  
+    } else if (diff === "hard") {
+      setLife(7);
+      setMaxRandomNumber(100); 
+    } else if (diff === "expert") {
+      setLife(5);
+      setMaxRandomNumber(80);
+    }
+  }, [diff]);
 
   useEffect(() => {
     generateNumber();
@@ -73,11 +94,12 @@ export default function Page() {
 
   return (
     <div className="h-[100vh] w-[100vw] bg-cover bg-center bg-[url('/ressources/numberthing/bg.jpg')]  overflow-hidden">
+      <MouseFollower lives={life} />
       {/* Main box */}
-      <div className="mx-auto my-auto border-4 border-white rounded-[10px] w-[35vw] min-w-[400px] h-[35vh] min-h-[300px] bg-cover bg-center bg-[url('/ressources/numberthing/grass.png')] mt-[20vh]">
-        <div className="mx-auto mt-[20px] text-center">
+      <div className="mx-auto my-auto border-4 border-white rounded-[15px] w-[50vw] min-w-[400px] h-[35vw] min-h-[400px] bg-cover bg-center bg-[url('/ressources/numberthing/grass.png')] mt-[20vh] flex">
+        <div className="mx-auto my-auto text-center">
           <p className="font-extrabold text-2xl text-white">
-            Guess the number between 1 and 100
+            Guess the number between 1 and {maxRandomNumber}
           </p>
 
           <form onSubmit={submitCallback} className="mt-[10px]">
@@ -87,12 +109,14 @@ export default function Page() {
               id="myInput"
               value={numberGuess}
               onChange={onChange}
+              disabled={ded}
             />
           </form>
 
           <button
             onClick={submitCallback}
             className="mx-auto mt-[10px] text-[20px] text-white border-[1.5px] rounded-[5px] p-1 px-4 hover:text-white duration-500 border-white hover:backdrop-blur-sm"
+            disabled={ded}
           >
             Submit
           </button>
@@ -104,16 +128,25 @@ export default function Page() {
           )}
         </div>
       </div>
-
-      {/* Lives */}
-      <div className="flex justify-center items-center mt-4">
-        {Array.from({ length: life }).map((_, i) => (
-          <div
-            key={i}
-            className="bg-[url('/ressources/numberthing/burgy.png')] bg-contain bg-no-repeat h-[120px] w-[150px]"
-          />
-        ))}
+      {ded &&(
+      <div className="absolute w-[80vw] h-[80vh] bg-black flex top-10 left-1/10">
+        <div className="mx-auto my-auto grid">
+          <p className="text-7xl font-extrabold text-red-600">You Died!</p>
+          <button
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[50px] text-center"
+          onClick={button2submitCallback}
+          >
+            Respawn
+          </button>
+          <button
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[50px] text-center"
+          onClick={button2submitCallback}
+          >
+            Title Screen
+          </button>
+        </div>
       </div>
+    )}
     </div>
   );
 }
