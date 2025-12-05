09047da975ce01bc88d02e12807797944a901f33
Yuan Guanwei <2221661@cje.qc.ca>
2025-12-05 14:58:45 -0500

    did shit

--- README.md
+++ README.md
@@ -31,25 +31,8 @@ npm run start
 Webpage will typically up on `localhost:3000` unless the port is taken
 
 --------------------------------------------------------
-### FOR THE FORMULAIRE
+### FOR THE GAME
 
-Open `localhost:3000` to see the webpage (after installing the requirements & ran npm run dev)
-
-The source code is in app/page.tsx and app/registered/*, the global.css is automatically generated, layout.tsx contains the basic settings of the site (description, page name)
-
-All the code until the return function (line 162) is the typescript used to validate password, handle form and redirect, the html and css is from line 162
-
-Im using tailwind css for styling, https://tailwindcss.com/ so the css is directly in the html, eg: className="bg-black" = background black or text-white = white text
-
-If you want to see the css code (like .bg-black {background-color: var(--color-black)}), install tailwind extension in vscode and hover over the classname
-
-Some varaibles arent really named properly, refer to the comments for their actual functionality
-
-I used prettier for code format https://prettier.io/
-
-where i used gpt:
-- to prevent full refresh when submiting the form line 
-- to add/remove items from the reason array 
 
 other resources used:
 stackoverflow 
@@ -58,19 +41,13 @@ nextjs docs
 react docs
 
 
-The easter egg:
-when username is set as bounce, and the form is submitted, the form will start bouncing
-submit the form again with the username anything else then bounce to stop it from bouncing
 
 --------------------------------------------------------
 Added features:
 ```
-- easter egg when entering username bounce and submitting
-- animation on hover (register div) line 116
-- error message 
-- display live password eligibility instead of only displaying it on submit (most important feature ig)
-- detection for special character
-- detection for minimum 8 characters
-- actual redirected page (/registered) with username displayed
-- uses html directly in the page to indicate requirments
+
 ```
+external used sources:
+https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+https://stackoverflow.com/questions/47686345/playing-sound-in-react-js
+https://stackoverflow.com/questions/74540010/how-do-i-dynamically-add-a-div-where-mouse-was-clicked-in-react (helped the hearts that follows the mouse)
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,71 +3,113 @@
 import React, { useEffect, useState } from "react";
 // for redirecting
 import { useRouter, useSearchParams } from "next/navigation";
+// imports the heart count that follows the mouse
 import MouseFollower from "../components/mousefollower";
-
+import useSound from 'use-sound';
 export default function Page() {
+  const winSound = new Audio("/ressources/audio/win.mp3");
+  const hurtSound = new Audio("/ressources/audio/hurt.mp3");
+  const WarningSound = new Audio("/ressources/audio/warning.mp3");
+  const clickSound = new Audio("/ressources/audio/click.mp3");
+  const deadSound = new Audio("/ressources/audio/ded.mp3");
+  // all the vars
+  const router = useRouter();
+  // gets the difficulty after redirect
   const searchParams = useSearchParams();
   const diff = searchParams.get("diff");
+  if (!diff) {
+    router.push("/difficultySelect");
+  } else if (
+    diff !== "easy" &&
+    diff !== "medium" &&
+    diff !== "hard" &&
+    diff !== "expert"
+  ) {
+    router.push("/difficultySelect");
+  }
+  // life
   const [life, setLife] = useState<number>(8);
+  // if the user guessed the thing or not
   const [guessed, setGuessed] = useState<boolean>(false);
+  // the randomly generated number
   const [number, setNumber] = useState<number>(0);
-  const [numberGuess, setNumberGuess] = useState<string>("");
+  // the guess that the player made
+  const [numberGuess, setNumberGuess] = useState<string>("")
   const [message, setMessage] = useState<string>("");
+  // the maximum of the generated number, changes on difficulty
   const [maxRandomNumber, setMaxRandomNumber] = useState<number>(100);
+  // if the player failed or not
   const [ded, setded] = useState<boolean>(false);
-  const router = useRouter();
-
+  const [tried, settried] = useState<number>(0);
+  
+  // generates a random number https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
   const getRandomInteger = (): number => {
     return Math.floor(Math.random() * maxRandomNumber) + 1; // 1 - maxRandomNumber
   };
-
+  
   const generateNumber = () => {
     const newNumber = getRandomInteger();
     setNumber(newNumber);
     console.log(newNumber);
   };
-
+  // when the input changes
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const v = e.target.value;
     setNumberGuess(v);
     console.log(v); 
   };
+  // reloads the page
   const button2submitCallback = () => {
-    router.push("/difficultySelect");
+    clickSound.play();
+    window.location.reload();
   }
+  // title screen button
+  const button3submitCallback = () => {
+    clickSound.play();
+    router.push("/difficultySelect");
+    
+  }
+  // goes to next difficulty
+  const button4submitCallback = () => {
+    clickSound.play();
+    if (diff === "easy") router.push("/game?diff=medium");
+    else if (diff === "medium") router.push("/game?diff=hard");
+    else if (diff === "hard") router.push("/game?diff=expert");
+    else if (diff === "expert") router.push("/game?diff=easy");
+    window.location.reload();
+  }
+  // when the number is submitted
   const submitCallback = (e: React.FormEvent) => {
     e.preventDefault();
-
+    // parses the number
     const parsed = parseInt(numberGuess.trim(), 10);
-
+    // if the number is not valid
     if (Number.isNaN(parsed)) {
       setMessage("Set a valid number!");
       return;
     }
 
     if (parsed === number) {
-      setMessage("You guessed it!");
+      winSound.play();
       setGuessed(true);
-      generateNumber();
-      setLife(8);
-      setNumberGuess("");
-      setTimeout(() => {
-        router.push("/won?tried=" + String(8-life+1));
-      },1000);
       return;
     }
-
-
+    settried(tried + 1);
     const newLife = life - 1;
     setLife(newLife);
-
+    
+    if (newLife === 1) {
+      WarningSound.play();
+    } else {
+      hurtSound.play();
+    }
     if (parsed < number) {
       setMessage("Guess is smaller than the number!");
     } else {
       setMessage("Guess is bigger than the number!");
     }
-
     if (newLife <= 0) {
+      deadSound.play();
       setded(true)
     }
   };
@@ -75,7 +117,7 @@ export default function Page() {
   useEffect(() => {
     if (diff === "easy") {
       setLife(12);
-      setMaxRandomNumber(60);
+      setMaxRandomNumber(70);
     } else if (diff === "medium") {
       setLife(9);
       setMaxRandomNumber(100);  
@@ -84,7 +126,7 @@ export default function Page() {
       setMaxRandomNumber(100); 
     } else if (diff === "expert") {
       setLife(5);
-      setMaxRandomNumber(80);
+      setMaxRandomNumber(85);
     }
   }, [diff]);
 
@@ -115,7 +157,7 @@ export default function Page() {
 
           <button
             onClick={submitCallback}
-            className="mx-auto mt-[10px] text-[20px] text-white border-[1.5px] rounded-[5px] p-1 px-4 hover:text-white duration-500 border-white hover:backdrop-blur-sm"
+            className="shadow-lg mx-auto mt-[10px] text-[20px] text-white border-[1.5px] rounded-[5px] p-1 px-4 hover:text-white duration-500 border-white hover:backdrop-blur-sm "
             disabled={ded}
           >
             Submit
@@ -129,18 +171,43 @@ export default function Page() {
         </div>
       </div>
       {ded &&(
-      <div className="absolute w-[80vw] h-[80vh] bg-black flex top-10 left-1/10">
+      <div className="absolute w-[100vw] h-[100vh] bg-black flex z-100 top-0">
         <div className="mx-auto my-auto grid">
           <p className="text-7xl font-extrabold text-red-600">You Died!</p>
+          <p className="font-bold text-white pt-5">
+            The number was: {number}
+          </p>
           <button
-          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[50px] text-center"
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[30px] text-center"
           onClick={button2submitCallback}
           >
             Respawn
           </button>
           <button
-          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[50px] text-center"
-          onClick={button2submitCallback}
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[40px] text-center"
+          onClick={button3submitCallback}
+          >
+            Title Screen
+          </button>
+        </div>
+      </div>
+    )}
+    {guessed &&(
+      <div className="absolute w-[100vw] h-[100vh] bg-black flex z-100 top-0">
+        <div className="mx-auto my-auto grid">
+          <p className="text-7xl font-extrabold text-green-600">You Guessed It!</p>
+          <p className="font-bold text-white pt-5">
+            Congrats! You guessed the number {number} in {tried} tries!
+          </p>
+          <button
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[30px] text-center"
+          onClick={button4submitCallback}
+          >
+            Next difficulty
+          </button>
+          <button
+          className="p-2 px-7  bg-gray-500 text-black font-bold rounded-[1px] hover:scale-105 duration-300 hover:bg-gray-400 w-[500px] mt-[40px] text-center"
+          onClick={button3submitCallback}
           >
             Title Screen
           </button>
